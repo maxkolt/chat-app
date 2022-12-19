9f96841174f59b4088820a2063ca46930c496f7d
maximkoltovich <12345kolt@gmail.com>
2022-12-19 22:07:19 +0300

    Работа над приложением

---
+++ src/components/contacts/contact-user-info.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "contact-user-info"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/main-wrapper.vue
+++ src/components/main-wrapper.vue
@@ -1,15 +1,21 @@
 <template>
   <div class='main-wrapper'>
-
+    <v-header/>
+    <router-view/>
+    <bottom-nav/>
   </div>
 </template>
 
 <script>
 
+import vHeader from './layouts/v-header.vue'
+import bottomNav from './layouts/bottom-nav.vue'
+
 export default {
   name: "main-wrapper",
   components: {
-
+    vHeader,
+    bottomNav
   },
   props: {},
   data() {
